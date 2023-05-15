fc600146d45a426f5519786080c50230d4b7dabb
otterrr <b211119012@gmail.com>
2023-05-16 01:13:34 +0800

    config env

--- util/database.js
+++ util/database.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
     process.env.PG_DB,
     process.env.PG_USER,
-    process.env.PG_PASSORD,
+    process.env.PG_PASSWORD,
     {
         host: process.env.PG_HOST,
         dialect:'postgres',
